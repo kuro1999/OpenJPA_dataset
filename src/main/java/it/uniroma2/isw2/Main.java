ce23f46f4d044b3e01c221c804f28e10ed54f502
kuro1999 <edoardo.marchionni99@gmail.com>
2026-04-17 15:09:30 +0200

    IT BEGINS

---
+++ src/main/java/it/uniroma2/isw2/Main.java
@@ -0,0 +1,10 @@
+package it.uniroma2.isw2;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Avvio estrazione release OPENJPA");
+    }
+}
---
+++ src/test/java/it/uniroma2/isw2/AppTest.java
@@ -0,0 +1,19 @@
+package it.uniroma2.isw2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
